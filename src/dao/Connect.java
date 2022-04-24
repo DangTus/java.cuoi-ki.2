93b8db1cde02108d01901d796d999e3e892102db
DangTus <dangtu9520@gmail.com>
2022-04-24 08:02:45 +0700

    dua file len

---
+++ src/dao/Connect.java
@@ -0,0 +1,25 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Connect {
+
+    public static Connection getJDBCConnection() {
+        final String url = "jdbc:mysql://localhost:3306/java.quanlithuvien";
+        final String user = "root";
+        final String password = "";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(url, user, password);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(Connect.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+}
